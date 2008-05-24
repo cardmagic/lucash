dc6730db300082e425102f194424443ab4f052f4
Lucas Carlson <lucas@rufy.com>
2008-05-23 20:33:54 -0700

    Fixing rspec base

---
+++ Rakefile
@@ -0,0 +1,36 @@
+# NOTE! When updating this file, also update INSTALL, if necessary.
+# NOTE! Please keep your tasks grouped together.
+
+LUCASH_BASE = File.expand_path(File.dirname(__FILE__))
+
+task :default => :build
+
+# BUILD TASKS
+
+desc "Build everything that needs to be built"
+task :build => 'build:all'
+
+namespace :build do
+
+  task :all => %w[
+    build:grammar
+  ]
+
+  desc "Compiles the grammar (with racc)"
+  task :grammar do
+    Dir.chdir(LUCASH_BASE + "/lib/lucash")
+    `racc -o grammar.rb grammar.y`
+  end
+end
+
+require 'spec/rake/spectask'
+
+desc "Run all specs"
+Spec::Rake::SpecTask.new do |t|
+  t.spec_files = FileList[
+    'spec/**/*_spec.rb'
+  ]
+  t.rcov = true
+  t.rcov_dir = '../doc/output/coverage'
+  t.rcov_opts = ['--exclude', 'spec\/spec,bin\/spec,examples']
+end
--- lib/lucash/ast.rb
+++ lib/lucash/ast.rb
@@ -1,62 +1,73 @@
-$vals = {}
-
-def lucash_eval(ast)
-  case ast[0]
-  when :program
-    ast[1].map {|stmt| lucash_eval(stmt)}.last
-  when :line
-    lucash_eval(ast[1])
-  when :and
-    if lucash_eval(ast[1])
-      lucash_eval(ast[2])
+class Lucash
+  class AST
+    def initialize(ast)
+      @vars = {}
+      @ast = ast
     end
-  when :if
-    if lucash_eval(ast[1])
-      lucash_eval(ast[2])
+    
+    def eval
+      lucash_eval(@ast)
     end
-  when :if_else
-    if lucash_eval(ast[1])
-      lucash_eval(ast[2])
-    else
-      lucash_eval(ast[3])
+    
+    def lucash_eval(ast)
+      case ast[0]
+      when :program
+        ast[1].map {|stmt| lucash_eval(stmt)}.last
+      when :line
+        lucash_eval(ast[1])
+      when :and
+        if lucash_eval(ast[1])
+          lucash_eval(ast[2])
+        end
+      when :if
+        if lucash_eval(ast[1])
+          lucash_eval(ast[2])
+        end
+      when :if_else
+        if lucash_eval(ast[1])
+          lucash_eval(ast[2])
+        else
+          lucash_eval(ast[3])
+        end
+      when :for
+        for i in (lucash_eval(ast[1])..lucash_eval(ast[2]))
+          @vars[ast[3]] = i
+          lucash_eval(ast[4])
+        end
+      when :assignment
+        @vars[ast[1]] = lucash_eval(ast[2])
+      when :functional_assignment
+        @vars[ast[1]] = ast[2]
+      when :add
+        lucash_eval(ast[1]) + lucash_eval(ast[2])
+      when :block
+        lucash_eval(ast[1])
+      when :subtract
+        lucash_eval(ast[1]) - lucash_eval(ast[2])
+      when :multiply
+        lucash_eval(ast[1]) * lucash_eval(ast[2])
+      when :divide
+        lucash_eval(ast[1]) / lucash_eval(ast[2])
+      when :mod
+        lucash_eval(ast[1]) % lucash_eval(ast[2])
+      when :array
+        lucash_eval(ast[1])
+      when :splat
+        ast[1].map{|a| lucash_eval(a)}
+      when :string
+        ast[1]
+      when :value
+        case ast[1][0]
+        when "false"
+          false
+        when "true"
+          true
+        else
+          ast[1]
+        end
+      when :number
+        ast[1]
+      end
     end
-  when :for
-    for i in (lucash_eval(ast.from)..lucash_eval(ast.to))
-      $vals[ast.ident.lexeme] = i
-      lucash_eval(ast.statements)
-    end
-  when :assignment
-    $vals[ast[1]] = lucash_eval(ast[2])
-  when :functional_assignment
-    $vals[ast[1]] = ast[2]
-  when :add
-    lucash_eval(ast[1]) + lucash_eval(ast[2])
-  when :block
-    lucash_eval(ast[1])
-  when :subtract
-    lucash_eval(ast[1]) - lucash_eval(ast[2])
-  when :multiply
-    lucash_eval(ast[1]) * lucash_eval(ast[2])
-  when :divide
-    lucash_eval(ast[1]) / lucash_eval(ast[2])
-  when :mod
-    lucash_eval(ast[1]) % lucash_eval(ast[2])
-  when :array
-    lucash_eval(ast[1])
-  when :splat
-    ast[1].map{|a| lucash_eval(a)}
-  when :string
-    ast[1]
-  when :value
-    case ast[1][0]
-    when "false"
-      false
-    when "true"
-      true
-    else
-      ast[1]
-    end
-  when :number
-    ast[1]
   end
 end
