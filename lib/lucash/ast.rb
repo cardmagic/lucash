c497d4c4c240117cb682b85249ddffef46d97296
Lucas Carlson <lucas@rufy.com>
2008-06-06 16:58:30 -0700

    Revert "Adding first specs"
    This reverts commit c896fae7d541f9b0a2beeede158c8532aef6e89c.

--- lib/lucash/ast.rb
+++ lib/lucash/ast.rb
@@ -1,80 +1,62 @@
-class Lucash
-  class Ast
-    class InvalidAst < StandardError; end
-    
-    attr_accessor :ast
-    
-    def initialize(ast=nil)
-      @vals = {}
-      @ast = ast
+$vals = {}
+
+def lucash_eval(ast)
+  case ast[0]
+  when :program
+    ast[1].map {|stmt| lucash_eval(stmt)}.last
+  when :line
+    lucash_eval(ast[1])
+  when :and
+    if lucash_eval(ast[1])
+      lucash_eval(ast[2])
     end
-    
-    def eval
-      raise InvalidAst unless @ast.is_a?(Array)
-      lucash_eval(@ast)
+  when :if
+    if lucash_eval(ast[1])
+      lucash_eval(ast[2])
     end
-    
-    def lucash_eval(ast)
-      case ast[0]
-      when :program
-        ast[1].map {|stmt| lucash_eval(stmt)}.last
-      when :line
-        lucash_eval(ast[1])
-      when :and
-        if lucash_eval(ast[1])
-          lucash_eval(ast[2])
-        end
-      when :if
-        if lucash_eval(ast[1])
-          lucash_eval(ast[2])
-        end
-      when :if_else
-        if lucash_eval(ast[1])
-          lucash_eval(ast[2])
-        else
-          lucash_eval(ast[3])
-        end
-      when :for
-        for i in (lucash_eval(ast.from)..lucash_eval(ast.to))
-          @vals[ast.ident.lexeme] = i
-          lucash_eval(ast.statements)
-        end
-      when :assignment
-        @vals[ast[1]] = lucash_eval(ast[2])
-      when :functional_assignment
-        @vals[ast[1]] = ast[2]
-      when :add
-        lucash_eval(ast[1]) + lucash_eval(ast[2])
-      when :block
-        lucash_eval(ast[1])
-      when :subtract
-        lucash_eval(ast[1]) - lucash_eval(ast[2])
-      when :multiply
-        lucash_eval(ast[1]) * lucash_eval(ast[2])
-      when :divide
-        lucash_eval(ast[1]) / lucash_eval(ast[2])
-      when :mod
-        lucash_eval(ast[1]) % lucash_eval(ast[2])
-      when :array
-        lucash_eval(ast[1])
-      when :splat
-        ast[1].map{|a| lucash_eval(a)}
-      when :string
-        ast[1]
-      when :value
-        case ast[1][0]
-        when "false"
-          false
-        when "true"
-          true
-        else
-          ast[1]
-        end
-      when :number
-        ast[1]
-      else
-        raise InvalidAst
-      end
+  when :if_else
+    if lucash_eval(ast[1])
+      lucash_eval(ast[2])
+    else
+      lucash_eval(ast[3])
     end
+  when :for
+    for i in (lucash_eval(ast.from)..lucash_eval(ast.to))
+      $vals[ast.ident.lexeme] = i
+      lucash_eval(ast.statements)
+    end
+  when :assignment
+    $vals[ast[1]] = lucash_eval(ast[2])
+  when :functional_assignment
+    $vals[ast[1]] = ast[2]
+  when :add
+    lucash_eval(ast[1]) + lucash_eval(ast[2])
+  when :block
+    lucash_eval(ast[1])
+  when :subtract
+    lucash_eval(ast[1]) - lucash_eval(ast[2])
+  when :multiply
+    lucash_eval(ast[1]) * lucash_eval(ast[2])
+  when :divide
+    lucash_eval(ast[1]) / lucash_eval(ast[2])
+  when :mod
+    lucash_eval(ast[1]) % lucash_eval(ast[2])
+  when :array
+    lucash_eval(ast[1])
+  when :splat
+    ast[1].map{|a| lucash_eval(a)}
+  when :string
+    ast[1]
+  when :value
+    case ast[1][0]
+    when "false"
+      false
+    when "true"
+      true
+    else
+      ast[1]
+    end
+  when :number
+    ast[1]
   end
 end
--- lib/lucash/grammar.y
+++ lib/lucash/grammar.y
@@ -8,19 +8,19 @@ class Lucash
 rule
 	program: 
 		  line program { [:program, [val[0], *val[1][1]]] }
-		| '{' program '}' { [:block, val[1]] }
+    | '{' program '}' { [:block, val[1]] }
 		| line { [:program, [val[0]]] }
-		| '\n' { [:program, []] }
-		| ';' { [:program, []] }
+		| program '\n' { [:program, [val[0]]] }
+		| program ';' { [:program, [val[0]]] }
 	line: 
 	    expr { [:line, val[0]] } 
 		| expr ';' { [:line, val[0]] } 
 		| expr '\n' { [:line, val[0]] } 
 		| '(' line ')' { [:line, val[1]] }
-		| '(' ')' { [:empty_parens] }
+    | '(' ')' { [:empty_parens] }
 		| 'if' line program 'end' { [:if, val[1], val[2]] }
 		| 'if' line program 'else' program 'end' { [:if_else, val[1], val[2], val[4]] }
-		| expr '&&' line { [:and, val[0], val[2]] }
+	  | expr '&&' line { [:and, val[0], val[2]] }
 		| command '|' line { [:pipe, [:line, val[0]], val[2]] }
 		| line '.' line { [:method, val[0], val[2]] }
 		| line '.' IDENT { [:method, val[0], val[2]] }
@@ -37,17 +37,17 @@ rule
 		| array { val[0] }
 	array:
 		  '[' basic_result ']'	{ [:array, val[1]] }
-		| '[' ']'		 		        { [:empty_array] }
+    | '[' ']'		 		        { [:empty_array] }
 		| atom					        { val[0] }
 	basic_result:
 		  program { [:splat, [val[0]]] }
 		| program ',' basic_result { [:splat, [val[0], *val[2][1]]] }
 	atom:
 	    NUMBER { [:number, val[0]] }
-		| command { val[0] }
+    | command { val[0] }
 	command:
 		  IDENT { [:value, [val[0]]] }
-		| IDENT command { [:value, [val[0], *val[1][1]]] }
+  	| IDENT command { [:value, [val[0], *val[1][1]]] }
 end
 
 ---- inner
